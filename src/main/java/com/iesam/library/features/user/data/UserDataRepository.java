813e84e13e88a9b400e9caeb84914e0cfcce904d
DavidLozSan <davidloz94.dls@gmail.com>
2024-04-18 00:23:53 +0200

    Solucion de errores

--- src/main/java/com/iesam/library/features/user/data/UserDataRepository.java
+++ src/main/java/com/iesam/library/features/user/data/UserDataRepository.java
@@ -1,14 +1,26 @@
 package com.iesam.library.features.user.data;
 
+import com.iesam.library.features.user.data.local.UserMemLocalDataSource;
 import com.iesam.library.features.user.domain.User;
 import com.iesam.library.features.user.domain.UserRepository;
 import com.iesam.library.features.user.data.local.UserFileLocalDataSource;
 
 public class UserDataRepository implements UserRepository {
     private UserFileLocalDataSource userFileLocalDataSource;
+    private UserMemLocalDataSource userMemLocalDataSource;
 
+    public UserDataRepository () {
+        this.userFileLocalDataSource = new UserFileLocalDataSource();
+    }
     @Override
     public void save(User user) {
         userFileLocalDataSource.save(user);
     }
+    /*
+    public UserDataRepository() {
+        this.userFileLocalDataSource = new UserFileLocalDataSource();
+    }
+    public UserDataRepository(UserFileLocalDataSource userFileLocalDataSource) {
+        this.userFileLocalDataSource = userFileLocalDataSource;
+    }*/
 }
