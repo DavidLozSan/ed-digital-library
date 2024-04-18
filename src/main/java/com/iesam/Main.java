c319fb684bdc30395abed6bb57ffc1cfb42adc19
DavidLozSan <davidloz94.dls@gmail.com>
2024-04-18 17:09:49 +0200

    Modificacion de ubicacion de formulario, del Main a UserPresentation

--- src/main/java/com/iesam/Main.java
+++ src/main/java/com/iesam/Main.java
@@ -18,23 +18,7 @@ public class Main {
             Integer opcion = sc.nextInt();
             switch (opcion) {
                 case 1:
-                    sc.nextLine();
-                    System.out.println("Dame el codigo del usuario");
-                    String codigo = sc.nextLine();
-                    System.out.println("Dame el dni del usuario");
-                    String dni = sc.nextLine();
-                    System.out.println("Dame el nombre del usuario");
-                    String nombre = sc.nextLine();
-                    System.out.println("Dame el apellidos del usuario");
-                    String apellidos = sc.nextLine();
-                    System.out.println("Dame el fecha de expedición del usuario");
-                    String fechaExped = sc.nextLine();
-                    System.out.println("Dame el email del usuario");
-                    String email = sc.nextLine();
-                    System.out.println("Dame el telefono del usuario");
-                    String telefono = sc.nextLine();
-                    User u1 = new User(codigo, dni, nombre, apellidos, fechaExped, email, telefono);
-                    UserPresentation.save(u1);
+                    UserPresentation.save();
                     break;
                 case 0:
                     bucle = false;
--- src/main/java/com/iesam/library/features/user/presentation/UserPresentation.java
+++ src/main/java/com/iesam/library/features/user/presentation/UserPresentation.java
@@ -5,9 +5,27 @@ import com.iesam.library.features.user.data.local.UserFileLocalDataSource;
 import com.iesam.library.features.user.domain.SaveUserUseCase;
 import com.iesam.library.features.user.domain.User;
 
+import java.util.Scanner;
+
 public class UserPresentation {
-    public static void save(User user) {
+    public static void save() {
         SaveUserUseCase saveUserUseCase = new SaveUserUseCase(new UserDataRepository(new UserFileLocalDataSource()));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dame el codigo del usuario");
+        String codigo = sc.nextLine();
+        System.out.println("Dame el dni del usuario");
+        String dni = sc.nextLine();
+        System.out.println("Dame el nombre del usuario");
+        String nombre = sc.nextLine();
+        System.out.println("Dame el apellidos del usuario");
+        String apellidos = sc.nextLine();
+        System.out.println("Dame el fecha de expedición del usuario");
+        String fechaExped = sc.nextLine();
+        System.out.println("Dame el email del usuario");
+        String email = sc.nextLine();
+        System.out.println("Dame el telefono del usuario");
+        String telefono = sc.nextLine();
+        User user = new User(codigo, dni, nombre, apellidos, fechaExped, email, telefono);
         saveUserUseCase.execute(user);
     }
 }
