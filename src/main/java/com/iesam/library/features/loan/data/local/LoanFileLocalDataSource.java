8e252925caab67f7b52d999e22834181954641bd
DavidLozSan <davidloz94.dls@gmail.com>
2024-05-06 13:46:51 +0200

    Añadida funcionalidad para solicitar préstamos en la biblioteca

---
+++ src/main/java/com/iesam/library/features/loan/data/local/LoanFileLocalDataSource.java
@@ -0,0 +1,96 @@
+package com.iesam.library.features.loan.data.local;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.iesam.library.features.loan.domain.Loan;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class LoanFileLocalDataSource implements LoanLocalDataSource {
+
+    private String nameFile = "loan.txt";
+
+    private Gson gson = new Gson();
+
+    private final Type typeList = new TypeToken<ArrayList<Loan>>() {
+    }.getType();
+
+    public void save(Loan model) {
+        List<Loan> models = findAll();
+        models.add(model);
+        saveToFile(models);
+    }
+
+    public void saveList(List<Loan> models) {
+        saveToFile(models);
+    }
+
+    private void saveToFile(List<Loan> models) {
+        try {
+            FileWriter myWriter = new FileWriter(nameFile);
+            myWriter.write(gson.toJson(models));
+            myWriter.close();
+            System.out.println("Datos guardados correctamente");
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error al guardar la información.");
+            e.printStackTrace();
+        }
+    }
+
+    public Loan findByCode(String code) {
+        List<Loan> models = findAll();
+        for (Loan model : models) {
+            if (Objects.equals(model.getCode(), code)) {
+                return model;
+            }
+        }
+        return null;
+    }
+
+    public List<Loan> findAll() {
+        try {
+            File myObj = new File(nameFile);
+            if (!myObj.exists()) {
+                myObj.createNewFile();
+            }
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                myReader.close();
+                return gson.fromJson(data, typeList);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ha ocurrido un error al obtener el listado.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error al crear el fichero.");
+            throw new RuntimeException(e);
+        }
+        return new ArrayList<>();
+    }
+
+    public void delete(String modelCode) {
+        List<Loan> newList = new ArrayList<>();
+        List<Loan> models = findAll();
+        for (Loan model : models) {
+            if (!model.getCode().equals(modelCode)) {
+                newList.add(model);
+            }
+        }
+        saveList(newList);
+    }
+
+    public void update(Loan loan) {
+        delete(loan.getCode());
+        save(loan);
+    }
+}
