15528e499b479784cfeef5d947878db96f3f4bee
DavidLozSan <davidloz94.dls@gmail.com>
2024-05-16 12:22:35 +0200

    chore: se ha cambiado la forma de comprobar el recurso digital al guardar un préstamo.

--- src/main/java/com/iesam/library/features/loan/domain/SaveLoanUseCase.java
+++ src/main/java/com/iesam/library/features/loan/domain/SaveLoanUseCase.java
@@ -5,7 +5,6 @@ import com.iesam.library.features.digitalCollection.domain.DigitalRepository;
 import com.iesam.library.features.digitalCollection.domain.book.domain.Book;
 import com.iesam.library.features.digitalCollection.domain.book.domain.BookRepository;
 import com.iesam.library.features.user.domain.User;
-import com.iesam.library.features.digitalCollection.domain.TypeDigitalCollection;
 import com.iesam.library.features.user.domain.UserRepository;
 
 public class SaveLoanUseCase {
@@ -27,10 +26,12 @@ public class SaveLoanUseCase {
 
         DigitalCollection digitalCollection = digitalRepository.obtain(digitalResourceCode);
 
-        if (digitalCollection.digitalResourceType == TypeDigitalCollection.BOOK) {
-            Book book = bookRepository.obtain(digitalResourceCode);
-            Loan loan = new Loan(loanCode, user, book);
-            this.loanRepository.save(loan);
+        switch (digitalCollection.digitalResourceType) {
+            case BOOK:
+                Book book = bookRepository.obtain(digitalResourceCode);
+                Loan bookLoan = new Loan(loanCode, user, book);
+                this.loanRepository.save(bookLoan);
+                break;
         }
 
     }
