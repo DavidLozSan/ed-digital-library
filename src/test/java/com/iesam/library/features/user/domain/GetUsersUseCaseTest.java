bc87354b8669d38b47bf6ecb6f5ad523c150c599
DavidLozSan <146753495+DavidLozSan@users.noreply.github.com>
2024-05-18 01:36:31 +0200

    Test/47/test_GetUsersUseCase (#48)
    * test: añadido test para cuando se ejecuta el caso de uso de mostrar lista de usuarios entonces devuelva el listado de usuarios
    
    * fix: solución de conflictos

---
+++ src/test/java/com/iesam/library/features/user/domain/GetUsersUseCaseTest.java
@@ -0,0 +1,45 @@
+package com.iesam.library.features.user.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetUsersUseCaseTest {
+    @Mock
+    UserRepository userRepository;
+    GetUsersUseCase getUsersUseCase;
+
+    @BeforeEach
+    void setUp() {
+        getUsersUseCase = new GetUsersUseCase(userRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        getUsersUseCase = null;
+    }
+
+    @Test
+    public void theUseCaseIsExecutedThenReturnsTheUserList() {
+        User user1 = new User("001", "77755522A", "David", "Apellidos",
+                "13/05/2024", "david@correo.es", "444222333");
+        User user2 = new User("002", "69255522B", "Laura",
+                "Apellidos", "13/05/2024", "laura@correo.es", "665221339");
+        List<User> expectedUserList = List.of(user1, user2);
+        Mockito.when(userRepository.obtainUsers()).thenReturn(expectedUserList);
+
+        List<User> userListReceived = getUsersUseCase.execute();
+
+        assertEquals(expectedUserList, userListReceived);
+
+    }
+}
