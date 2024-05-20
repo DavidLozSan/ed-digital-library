4e890b0fd9239c7417f7349dea31dce252b50eae
DavidLozSan <davidloz94.dls@gmail.com>
2024-05-20 19:36:58 +0200

    chore: cambiado el test del FinalizeLoanUseCase para verificar correctamente que se ejecuta el caso de uso.

--- src/test/java/com/iesam/library/features/loan/domain/FinalizeLoanUseCaseTest.java
+++ src/test/java/com/iesam/library/features/loan/domain/FinalizeLoanUseCaseTest.java
@@ -1,5 +1,8 @@
 package com.iesam.library.features.loan.domain;
 
+import com.iesam.library.features.digitalCollection.domain.DigitalCollection;
+import com.iesam.library.features.digitalCollection.domain.TypeDigitalCollection;
+import com.iesam.library.features.user.domain.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +12,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class FinalizeLoanUseCaseTest {
@@ -29,9 +33,16 @@ class FinalizeLoanUseCaseTest {
     @Test
     public void givenLoanCodeThenTheUseCaseFinalizesTheLoan() {
         String loanCode = "001";
+        User user = new User("001", "1234", "David", "surnames", "16/05/2024",
+                "correo", "777888555");
+        DigitalCollection digitalCollection = new DigitalCollection("001", TypeDigitalCollection.BOOK,
+                "Libro1");
+        Loan loan = new Loan(loanCode, user, digitalCollection);
+        Mockito.when(loanRepository.obtainLoan(loanCode)).thenReturn(loan);
 
         finalizeLoanUseCase.execute(loanCode);
 
-        Mockito.verify(loanRepository, Mockito.times(1)).finalizeLoan(loanCode);
+        Mockito.verify(loanRepository, Mockito.times(1)).obtainLoan(loanCode);
+        Mockito.verify(loanRepository, Mockito.times(1)).finalizeLoan(any(Loan.class));
     }
 }
