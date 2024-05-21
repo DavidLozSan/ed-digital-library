8811c3a781f1d43d7f318a2c1f2fd050952c2d7d
DavidLozSan <146753495+DavidLozSan@users.noreply.github.com>
2024-05-21 19:17:42 +0200

    Test/83/test finalize loan (#84)
    * test: añadido el test de FinalizeLoanUseCase.
    
    * chore: cambiado forma de crear un préstamo finalizado en el caso de uso para finalizar el préstamo que se busca por código.
    
    * chore: cambiado el test del FinalizeLoanUseCase para verificar correctamente que se ejecuta el caso de uso.
    
    * chore: cambiado el caso de uso utilizando LoanFactory a traves de constructor.
      fix: cambiado test FinalizeLoanUseCase según los cambios del caso de uso.

---
+++ src/test/java/com/iesam/library/features/loan/domain/FinalizeLoanUseCaseTest.java
@@ -0,0 +1,54 @@
+package com.iesam.library.features.loan.domain;
+
+import com.iesam.library.features.digitalCollection.domain.DigitalCollection;
+import com.iesam.library.features.digitalCollection.domain.TypeDigitalCollection;
+import com.iesam.library.features.user.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+class FinalizeLoanUseCaseTest {
+    @Mock
+    LoanRepository loanRepository;
+    @Mock
+    LoanFactory loanFactory;
+    FinalizeLoanUseCase finalizeLoanUseCase;
+
+    @BeforeEach
+    void setUp() {
+        finalizeLoanUseCase = new FinalizeLoanUseCase(loanRepository, loanFactory);
+    }
+
+    @AfterEach
+    void tearDown() {
+        finalizeLoanUseCase = null;
+    }
+
+    @Test
+    public void givenLoanCodeThenTheUseCaseFinalizesTheLoan() {
+        String loanCode = "001";
+        User user = new User("001", "1234", "David", "surnames", "16/05/2024",
+                "correo", "777888555");
+        DigitalCollection digitalCollection = new DigitalCollection("001", TypeDigitalCollection.BOOK,
+                "Libro1");
+        Loan loan = new Loan(loanCode, user, digitalCollection);
+        Loan loanFinalized = new Loan(loan.code, loan.user, loan.digitalCollection, loan.loanStartDate,
+                loan.loanEndDate);
+        Mockito.when(loanRepository.obtainLoan(loanCode)).thenReturn(loan);
+        Mockito.when(loanFactory.build(loan.code, loan.user, loan.digitalCollection, loan.loanStartDate,
+                loan.loanEndDate)).thenReturn(loanFinalized);
+
+        finalizeLoanUseCase.execute(loanCode);
+
+        Mockito.verify(loanRepository, Mockito.times(1)).obtainLoan(loanCode);
+        Mockito.verify(loanRepository, Mockito.times(1)).finalizeLoan(loanFinalized);
+    }
+}
