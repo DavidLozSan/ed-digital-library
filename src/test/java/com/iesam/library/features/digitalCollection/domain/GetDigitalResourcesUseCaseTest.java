b2a44e4d50d54844b3843d6319554448a3a9614a
DavidLozSan <146753495+DavidLozSan@users.noreply.github.com>
2024-05-21 16:04:30 +0200

    Test/59/test get digital resources (#60)
    * test: Añadidos test theUseCaseIsExecutedThenReturnsTheDigitalResourcesList
    
    * fix: solución de conflictos
    
    * fix: cambiado el test de GetDigitalResourcesUseCase para no comparar los listados directamente, y comparar si su tamaño es igual.

---
+++ src/test/java/com/iesam/library/features/digitalCollection/domain/GetDigitalResourcesUseCaseTest.java
@@ -0,0 +1,47 @@
+package com.iesam.library.features.digitalCollection.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetDigitalResourcesUseCaseTest {
+    @Mock
+    DigitalRepository digitalRepository;
+    GetDigitalResourcesUseCase getDigitalResourcesUseCase;
+
+    @BeforeEach
+    void setUp() {
+        getDigitalResourcesUseCase = new GetDigitalResourcesUseCase(digitalRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        getDigitalResourcesUseCase = null;
+    }
+
+    @Test
+    public void theUseCaseIsExecutedThenReturnsTheDigitalResourcesList() {
+        DigitalCollection digitalCollection1 = new DigitalCollection("001", TypeDigitalCollection.BOOK,
+                "Libro1");
+        DigitalCollection digitalCollection2 = new DigitalCollection("002", TypeDigitalCollection.MUSIC,
+                "Musica1");
+        List<DigitalCollection> expectedDigitalResourcesList = List.of(digitalCollection1, digitalCollection2);
+        Mockito.when(digitalRepository.obtainDigitalResources()).thenReturn(expectedDigitalResourcesList);
+
+        List<DigitalCollection> dititalResourcesListReceived = getDigitalResourcesUseCase.execute();
+
+        Assertions.assertEquals(dititalResourcesListReceived.size(), expectedDigitalResourcesList.size());
+        Assertions.assertEquals(dititalResourcesListReceived.get(0).code, "001");
+        Assertions.assertEquals(dititalResourcesListReceived.get(1).code, "002");
+    }
+}
